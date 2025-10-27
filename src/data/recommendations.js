ba88a38daecea6e29565fc721c66731613491fe3
Harshwardhan Atkare <81025973+Harshwardhan431@users.noreply.github.com>
2025-10-27 18:31:14 +0530

    Edit recommendation quote for conciseness
    Removed redundant phrase in recommendation quote.

--- src/data/recommendations.js
+++ src/data/recommendations.js
@@ -7,7 +7,7 @@ export const recommendationsData = [
     name: "Himanshu Pednekar",
     designation: "SDE @Morgan Stanley",
     quote:
-      "Harshwardhan is a hardworking guy who has the potential to manage a team, he has proved himself to be an important asset when it came to building real world applications by helping with the knowledge he has gained on working on his skill, he will give his best in any team he works with, may it be tech or non tech, he gets the work done.",
+      "Harshwardhan is a hardworking guy, he has proved himself to be an important asset when it came to building real world applications by helping with the knowledge he has gained on working on his skill, he will give his best in any team he works with, may it be tech or non tech, he gets the work done.",
     image: vedant,
     linkedin: "https://www.linkedin.com/in/himanshu-pednekar/",
   },
