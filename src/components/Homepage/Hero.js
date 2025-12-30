9d52536590738345857a6cad99bafc67367115f8
Harshwardhan Atkare <harshwardhanatkare@Harshwardhans-MacBook-Air.local>
2025-12-30 22:32:25 +0530

    profile pic name updated

--- src/components/Homepage/Hero.js
+++ src/components/Homepage/Hero.js
@@ -1,7 +1,7 @@
 import Fade from "react-reveal/Fade";
 import { Link } from "react-router-dom";
 import Typewriter from "typewriter-effect";
-import photo from "../../images/me1.jpeg";
+import photo from "../../images/me1.jpg";
 
 export default function Hero() {
   return (
